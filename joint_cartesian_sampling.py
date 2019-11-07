c1595406372e2674e07936a11da49a44ff4eecc7
Keshav Iyengar <keshav.iyengar94@gmail.com>
2019-11-07 19:26:12 +0000

    plot generation and eval steps change.

--- joint_cartesian_sampling.py
+++ joint_cartesian_sampling.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     env = gym.make("Distal-4-Tube-Reach-v0", ros_flag=False)
 
     num_tubes = 4
-    num_samples = 1000
+    num_samples = 10000
     alpha_joint_data = np.empty([num_samples, 2 * num_tubes])
     beta_joint_data = np.empty([num_samples, num_tubes])
     ext_joint_data = np.empty([num_samples, num_tubes])
@@ -37,7 +37,8 @@ if __name__ == '__main__':
             rot_joint_data[itr, :] = [joint_sample[0], joint_sample[2], joint_sample[4]]
             ext_joint_data[itr, :] = [joint_sample[1], joint_sample[3], joint_sample[5]]
         if num_tubes == 4:
-            beta_joint_data[itr, :] = [triplet_joint_sample[2], triplet_joint_sample[5], triplet_joint_sample[8], triplet_joint_sample[11]]
+            beta_joint_data[itr, :] = [triplet_joint_sample[2], triplet_joint_sample[5], triplet_joint_sample[8],
+                                       triplet_joint_sample[11]]
             alpha_joint_data[itr, :] = [triplet_joint_sample[0], triplet_joint_sample[1], triplet_joint_sample[3],
                                         triplet_joint_sample[4], triplet_joint_sample[6], triplet_joint_sample[7],
                                         triplet_joint_sample[9], triplet_joint_sample[10]]
@@ -46,49 +47,56 @@ if __name__ == '__main__':
 
     # Create into dataframe for easy visualization
     if num_tubes == 2:
-        beta_joint_df = pd.DataFrame(beta_joint_data, columns=['beta_0', 'beta_1'])
-        a1 = beta_joint_df.plot.hist(bins=12, alpha=0.5)
+        # beta_joint_df = pd.DataFrame(beta_joint_data, columns=['beta_0', 'beta_1'])
+        # a1 = beta_joint_df.plot.hist(bins=12, alpha=0.5)
 
-        alpha_joint_df = pd.DataFrame(alpha_joint_data, columns=['tube_0_0', 'tube_0_1', 'tube_1_0', 'tube_1_1'])
-        a2 = alpha_joint_df.plot.hist(bins=12, alpha=0.5)
+        # alpha_joint_df = pd.DataFrame(alpha_joint_data, columns=['tube_0_0', 'tube_0_1', 'tube_1_0', 'tube_1_1'])
+        # a2 = alpha_joint_df.plot.hist(bins=12, alpha=0.5)
 
-        ext_joint_df = pd.DataFrame(ext_joint_data, columns=['ext_0', 'ext_1'])
-        a3 = ext_joint_df.plot.hist(bins=12, alpha=0.5)
+        ext_joint_df = pd.DataFrame(ext_joint_data, columns=[r'$L_0 + \beta_0$', r'$L_1 + \beta_1$'])
+        a3 = ext_joint_df.plot.hist(bins=30, alpha=0.5)
 
-        rot_joint_df = pd.DataFrame(rot_joint_data, columns=['rot_0', 'rot_1'])
-        a4 = rot_joint_df.plot.hist(bins=12, alpha=0.5)
+        rot_joint_df = pd.DataFrame(rot_joint_data,
+                                    columns=[r'$\alpha_0$', r'$\alpha_1$'])
+        a4 = rot_joint_df.plot.hist(bins=30, alpha=0.5)
 
         cart_df = pd.DataFrame(cartesian_data, columns=['x', 'y', 'z'])
-        a5 = cart_df.plot.hist(bins=12, alpha=0.5)
+        a5 = cart_df.plot.hist(bins=30, alpha=0.5)
 
     if num_tubes == 3:
-        beta_joint_df = pd.DataFrame(beta_joint_data, columns=['beta_0', 'beta_1', 'beta_3'])
-        a1 = beta_joint_df.plot.hist(bins=12, alpha=0.5)
+        # beta_joint_df = pd.DataFrame(beta_joint_data, columns=['beta_0', 'beta_1', 'beta_3'])
+        # a1 = beta_joint_df.plot.hist(bins=12, alpha=0.5)
 
-        alpha_joint_df = pd.DataFrame(alpha_joint_data, columns=['tube_0_0', 'tube_0_1', 'tube_1_0', 'tube_1_1', 'tube_2_0', 'tube_2_1'])
-        a2 = alpha_joint_df.plot.hist(bins=12, alpha=0.5)
+        # alpha_joint_df = pd.DataFrame(alpha_joint_data,
+        #                               columns=['tube_0_0', 'tube_0_1', 'tube_1_0', 'tube_1_1', 'tube_2_0', 'tube_2_1'])
+        # a2 = alpha_joint_df.plot.hist(bins=12, alpha=0.5)
 
-        ext_joint_df = pd.DataFrame(ext_joint_data, columns=['ext_0', 'ext_1', 'ext_2'])
-        a3 = ext_joint_df.plot.hist(bins=12, alpha=0.5)
+        ext_joint_df = pd.DataFrame(ext_joint_data, columns=[r'$L_0 + \beta_0$', r'$L_1 + \beta_1$', r'$L_2 + \beta_2$'])
+        a3 = ext_joint_df.plot.hist(bins=30, alpha=0.5)
 
-        rot_joint_df = pd.DataFrame(rot_joint_data, columns=['rot_0', 'rot_1', 'rot_2'])
-        a4 = rot_joint_df.plot.hist(bins=12, alpha=0.5)
+        rot_joint_df = pd.DataFrame(rot_joint_data,
+                                    columns=[r'$\alpha_0$', r'$\alpha_1$', r'$\alpha_2$'])
+        a4 = rot_joint_df.plot.hist(bins=30, alpha=0.5)
 
         cart_df = pd.DataFrame(cartesian_data, columns=['x', 'y', 'z'])
-        a5 = cart_df.plot.hist(bins=12, alpha=0.5)
+        a5 = cart_df.plot.hist(bins=30, alpha=0.5)
 
     if num_tubes == 4:
-        beta_joint_df = pd.DataFrame(beta_joint_data, columns=['beta_0', 'beta_1', 'beta_3', 'beta_4'])
-        a1 = beta_joint_df.plot.hist(bins=12, alpha=0.5)
+        # beta_joint_df = pd.DataFrame(beta_joint_data, columns=[r'$\beta_0$', r'$\beta_1$', r'$\beta_2$', r'$\beta_3$'])
+        # a1 = beta_joint_df.plot.hist(bins=30, alpha=0.5)
 
-        alpha_joint_df = pd.DataFrame(alpha_joint_data, columns=['tube_0_0', 'tube_0_1', 'tube_1_0', 'tube_1_1', 'tube_2_0', 'tube_2_1', 'tube_3_0', 'tube_3_1'])
-        a2 = alpha_joint_df.plot.hist(bins=12, alpha=0.5)
+        # alpha_joint_df = pd.DataFrame(alpha_joint_data,
+        #                               columns=['tube_0_0', 'tube_0_1', 'tube_1_0', 'tube_1_1', 'tube_2_0', 'tube_2_1',
+        #                                        'tube_3_0', 'tube_3_1'])
+        # a2 = alpha_joint_df.plot.hist(bins=30, alpha=0.5)
 
-        ext_joint_df = pd.DataFrame(ext_joint_data, columns=['ext_0', 'ext_1', 'ext_2', 'ext_3'])
-        a3 = ext_joint_df.plot.hist(bins=12, alpha=0.5)
+        ext_joint_df = pd.DataFrame(ext_joint_data, columns=[r'$L_0 + \beta_0$', r'$L_1 + \beta_1$', r'$L_2 + \beta_2$',
+                                                             r'$L_3 + \beta_3$'])
+        a3 = ext_joint_df.plot.hist(bins=30, alpha=0.5)
 
-        rot_joint_df = pd.DataFrame(rot_joint_data, columns=['rot_0', 'rot_1', 'rot_2', 'rot_3'])
-        a4 = rot_joint_df.plot.hist(bins=12, alpha=0.5)
+        rot_joint_df = pd.DataFrame(rot_joint_data,
+                                    columns=[r'$\alpha_0$', r'$\alpha_1$', r'$\alpha_2$', r'$\alpha_3$'])
+        a4 = rot_joint_df.plot.hist(bins=30, alpha=0.5)
 
         cart_df = pd.DataFrame(cartesian_data, columns=['x', 'y', 'z'])
-        a5 = cart_df.plot.hist(bins=12, alpha=0.5)
+        a5 = cart_df.plot.hist(bins=30, alpha=0.5)
